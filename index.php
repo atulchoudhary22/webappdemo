e39f056a2b29e7e37dacf25dfe258e9b1a87b4d7
atulchoudhary22 <61445786+atulchoudhary22@users.noreply.github.com>
2024-07-01 10:07:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>This is XYZ Ecommerce Webpage</h1>
+<h1>This is sellmyprod Ecommerce Webpage</h1>
 <h1>Home Page</h1>
 <?php echo "Click to buy Products"; ?>
 <?php echo "Some enhancement is done on this website"; ?>

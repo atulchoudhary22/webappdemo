6d4c4f77d7cd0c7582cb942ee37f614de11776e1
atulchoudhary22 <61445786+atulchoudhary22@users.noreply.github.com>
2025-06-14 12:23:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>This is sellmyprod Ecommerce Webpage</h1>
 <h1>Home Page</h1>
 <?php echo "Click to buy Products"; ?>
-<?php echo "App service - US"; ?>
+<?php echo "App service - CI for PrecisionIT"; ?>
 <?php echo "Some enhancement is done on this website"; ?>
 </body>
 </html> 

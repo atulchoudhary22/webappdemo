72d56e9ce7af3fa53a086f95c25933f7a22c8f31
atulchoudhary22 <61445786+atulchoudhary22@users.noreply.github.com>
2025-11-03 21:14:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>This is sellmyprod Ecommerce Webpage</h1>
 <h1>Home Page</h1>
 <?php echo "Click to buy Products"; ?>
-<?php echo "App service - Noridian"; ?>
+<?php echo "App service - Eshop"; ?>
 <?php echo "Some enhancement is done on this website"; ?>
 </body>
 </html> 
